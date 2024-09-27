f701ef419b30523c5d48de2be0d5408b67704406
robrown <robrown@stratacache.com>
2024-09-27 15:29:32 -0400

    fix(moving the nasa-project): moving the nasa-project out of this repo into its own to simplify tracking
    You can find the nasa-project here:
    https://github.com/captain-rocket/nasa-project

--- projects/NASA-PROJECT/server/src/routes/planets/planets.controller.js
+++
@@ -1,8 +0,0 @@
-const { getAllplanets } = require('../../models/planets.model');
-
-async function httpGetAllplanets(req, res) {
-  return res.status(200).json(await getAllplanets());
-}
-module.exports = {
-  httpGetAllplanets,
-};
--- projects/NASA-PROJECT/server/src/routes/planets/planets.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const { httpGetAllplanets } = require('../planets/planets.controller');
-
-const planetsRouter = express.Router();
-
-planetsRouter.get('/', httpGetAllplanets);
-
-module.exports = planetsRouter;
