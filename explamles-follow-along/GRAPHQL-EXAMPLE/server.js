8f3f1a4b00973cffa4dc19a31e01ce2d8e36cfdc
robrown <robrown@stratacache.com>
2024-10-02 12:03:46 -0400

    feat(graphql schema): Designing An E-Commerce Schema
    Followed along the lecture designing the schema
    Using some documentation and research I was able get the data to show in graphiql
    I will include a screen shot in the merge request

--- explamles-follow-along/GRAPHQL-EXAMPLE/server.js
+++ explamles-follow-along/GRAPHQL-EXAMPLE/server.js
@@ -1,29 +1,89 @@
 const express = require('express');
-
-const { buildSchema } = require('graphql');
-const { createSchema, createYoga } = require('graphql-yoga');
+const { createYoga } = require('graphql-yoga');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 const app = express();
 const port = 3000;
 
 const resolvers = {
   Query: {
-    description: () => 'Red Shoe',
-    price: () => 42.99,
+    products: () => [
+      {
+        id: 'redshoes',
+        description: 'Red Shoes',
+        price: 59.99,
+      },
+      {
+        id: 'bluejeanpair',
+        description: 'Blue Jeans Pair',
+        price: 74.99,
+      },
+    ],
+    orders: () => [
+      {
+        date: '10-2-2004',
+        subtotal: 97.98,
+        items: [
+          {
+            product: {
+              id: 'redshoes',
+              description: 'Old Red Shoes',
+              price: 48.99,
+            },
+            quantity: 2,
+          },
+        ],
+      },
+    ],
+    reviews: () => [
+      {
+        rating: 5,
+        comment: 'These shoes are awesomely red!',
+      },
+    ],
   },
 };
-const schema = createSchema({
-  typeDefs: `type Query {
-  description: String
-  price: Float
-}`,
+const typeDefs = `
+  type Query {
+  products: [Product]
+  orders: [Order]
+  reviews: [Review]
+  }
+
+  type Product {
+  id: ID!
+  description: String!
+  reviews: [Review]
+  price: Float!
+  }
+
+  type Review {
+  rating: Int!
+  comment: String
+  }
+
+  type Order {
+  date: String!
+  subtotal: Float!
+  items: [OrderItem]
+  }
+
+  type OrderItem {
+  product: Product!
+  quantity: Int!
+  }
+`;
+
+const schema = makeExecutableSchema({
+  typeDefs,
   resolvers,
 });
 
 app.use(
   '/graphql',
   createYoga({
-    schema: schema,
+    schema,
+    graphiql: true,
   }),
 );
 
